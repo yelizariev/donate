96d7570c9a1ba2f2a8c0674139a8eb2465a6c26e
Mikhail Klementev <blame@dumpstack.io>
2020-01-14 02:36:33 +0000

    Update deps

--- deps.nix
+++ deps.nix
@@ -4,8 +4,17 @@
     fetch = {
       type = "git";
       url = "https://code.dumpstack.io/lib/cryptocurrency";
-      rev = "47594f50db5d6a24dfa0bf3d1698c53b7f509f9c";
-      sha256 = "1i6kb4gmjxxc3wllg1px1xqasyx1yaszp2mqaxzkdf9vd9jm40ss";
+      rev = "2e23e9eea68edcb4407831776408a7c6dceba0f4";
+      sha256 = "0746rcdk79ynfch571fiz3zqf83aqg3khgwv7apggxr85m42xi0c";
+    };
+  }
+  {
+    goPackagePath = "github.com/VictoriaMetrics/fastcache";
+    fetch = {
+      type = "git";
+      url = "https://github.com/VictoriaMetrics/fastcache";
+      rev = "b83c58d2fd5a8717ce6ad7a6bfd4941bae8e569d";
+      sha256 = "19bcgygs0kp76rb78l9q4p5dvj6a0dc6r8m7jabsxnyy900nfip6";
     };
   }
   {
@@ -27,6 +36,87 @@
     };
   }
   {
+    goPackagePath = "github.com/aristanetworks/goarista";
+    fetch = {
+      type = "git";
+      url = "https://github.com/aristanetworks/goarista";
+      rev = "5d8d36c240c9af0ccde364594dd5fae756790b63";
+      sha256 = "1wkm9cgw4rrpiyrcja1qykliy6nk4avpwlv6pb8hg3k7d8g8k7qf";
+    };
+  }
+  {
+    goPackagePath = "github.com/btcsuite/btcd";
+    fetch = {
+      type = "git";
+      url = "https://github.com/btcsuite/btcd";
+      rev = "f3ec13030e4e828869954472cbc51ac36bee5c1d";
+      sha256 = "1xxk8k8pqzy1is7afh0q9lfsrdb4yqzg3x6zvclh1nsv894mjw5f";
+    };
+  }
+  {
+    goPackagePath = "github.com/btcsuite/btcutil";
+    fetch = {
+      type = "git";
+      url = "https://github.com/btcsuite/btcutil";
+      rev = "e17c9730c422e7c745002430f2782b948b59c1c2";
+      sha256 = "1v4way147ha34dc00rjzqdm7l8jiqdawqr78j5sq5vbq0szgkws1";
+    };
+  }
+  {
+    goPackagePath = "github.com/cespare/xxhash/v2";
+    fetch = {
+      type = "git";
+      url = "https://github.com/cespare/xxhash";
+      rev = "d7df74196a9e781ede915320c11c378c1b2f3a1f";
+      sha256 = "0rl5rs8546zj1vzggv38w93wx0b5dvav7yy5hzxa8kw7iikv1cgr";
+    };
+  }
+  {
+    goPackagePath = "github.com/deckarep/golang-set";
+    fetch = {
+      type = "git";
+      url = "https://github.com/deckarep/golang-set";
+      rev = "699df6a3acf6867538e50931511e9dc403da108a";
+      sha256 = "1xh07xh50q9hmvg0abwiclbnwrxwzdqx4qqds6mg982h0fhqiadc";
+    };
+  }
+  {
+    goPackagePath = "github.com/elastic/gosigar";
+    fetch = {
+      type = "git";
+      url = "https://github.com/elastic/gosigar";
+      rev = "7aef3366157f2bfdf3e068f73ce7193573e88e0c";
+      sha256 = "1zgh3fqxj1wf5mpy74nf5hm65n0wvzqsn1mpjx5i8ci07cpq0qj5";
+    };
+  }
+  {
+    goPackagePath = "github.com/ethereum/go-ethereum";
+    fetch = {
+      type = "git";
+      url = "https://github.com/ethereum/go-ethereum";
+      rev = "e9e69d6e296d21a7c9c66024c9888a862027252e";
+      sha256 = "12hp9wacc4rjh45hw9vrs6fzv4q244qmhr791dwdva3qs6i9b1y5";
+    };
+  }
+  {
+    goPackagePath = "github.com/go-stack/stack";
+    fetch = {
+      type = "git";
+      url = "https://github.com/go-stack/stack";
+      rev = "2fee6af1a9795aafbe0253a0cfbdf668e1fb8a9a";
+      sha256 = "0wk25751ryyvxclyp8jdk5c3ar0cmfr8lrjb66qbg4808x66b96v";
+    };
+  }
+  {
+    goPackagePath = "github.com/golang/snappy";
+    fetch = {
+      type = "git";
+      url = "https://github.com/golang/snappy";
+      rev = "ff6b7dc882cf4cfba7ee0b9f7dcc1ac096c554aa";
+      sha256 = "1ifwb7h9srnvx4jr99bsdqpc9ry36l8m97fjjjhaznm7g6ib1wnr";
+    };
+  }
+  {
     goPackagePath = "github.com/google/go-github/v29";
     fetch = {
       type = "git";
@@ -45,6 +135,15 @@
     };
   }
   {
+    goPackagePath = "github.com/gorilla/websocket";
+    fetch = {
+      type = "git";
+      url = "https://github.com/gorilla/websocket";
+      rev = "95ba29eb981bbb27d92e1f70bf8a1949452d926b";
+      sha256 = "08lvc9l0qagyhyrjj6jkhpq3zapa5gqr966bm33nb4bc0pd38f48";
+    };
+  }
+  {
     goPackagePath = "github.com/mattn/go-sqlite3";
     fetch = {
       type = "git";
@@ -54,12 +153,57 @@
     };
   }
   {
+    goPackagePath = "github.com/miguelmota/go-ethereum-hdwallet";
+    fetch = {
+      type = "git";
+      url = "https://github.com/miguelmota/go-ethereum-hdwallet";
+      rev = "4d9d106de2bd756bee0ca6866b1a27cefcd01c1f";
+      sha256 = "1y2f9h32q5dpy0iahznh8qkz7n62gl28sv2yxwnbzx8jw4nzfcbk";
+    };
+  }
+  {
+    goPackagePath = "github.com/rs/cors";
+    fetch = {
+      type = "git";
+      url = "https://github.com/rs/cors";
+      rev = "fdcf4f9773b8d459d3ec085a8c91b34fcd17803d";
+      sha256 = "11ncyfq0l36126nmn57j7wdz29laf641s489czkjfjnvbgdnl6ic";
+    };
+  }
+  {
+    goPackagePath = "github.com/steakknife/bloomfilter";
+    fetch = {
+      type = "git";
+      url = "https://github.com/steakknife/bloomfilter";
+      rev = "6819c0d2a57025e1700378ee59b7382d71987f14";
+      sha256 = "1dr4pb172rwi9cxvxl23j4j8q6ybqjfzll5dd14cd398f231nk81";
+    };
+  }
+  {
+    goPackagePath = "github.com/steakknife/hamming";
+    fetch = {
+      type = "git";
+      url = "https://github.com/steakknife/hamming";
+      rev = "c99c65617cd3d686aea8365fe563d6542f01d940";
+      sha256 = "03scc8fkkwpbc7p9ymlrm6wg57mzr0d6s2k2dz2gp339pllxyxjr";
+    };
+  }
+  {
+    goPackagePath = "github.com/tyler-smith/go-bip39";
+    fetch = {
+      type = "git";
+      url = "https://github.com/tyler-smith/go-bip39";
+      rev = "c55f737395bc849274c1d1a35ca6f29a4e568092";
+      sha256 = "0h9l7fbp62akkmws7wf4ar8wswdvraa2g1m5fkj0wp6v2nj0qzgk";
+    };
+  }
+  {
     goPackagePath = "golang.org/x/crypto";
     fetch = {
       type = "git";
       url = "https://go.googlesource.com/crypto";
-      rev = "4def268fd1a49955bfb3dda92fe3db4f924f2285";
-      sha256 = "1bfsnari529gw34cz0zqk3d9mrkcj1ay35kangri8kbgll0ss5a6";
+      rev = "61a87790db17894570dfb32dbaa0a4af9ce60cb4";
+      sha256 = "17v9kj0fd13crzkz3nsr5xw1labv7qfjfhksbwic3jrw70kvmz9r";
     };
   }
   {
@@ -78,6 +222,15 @@
       url = "https://go.googlesource.com/oauth2";
       rev = "bf48bf16ab8d622ce64ec6ce98d2c98f916b6303";
       sha256 = "1sirdib60zwmh93kf9qrx51r8544k1p9rs5mk0797wibz3m4mrdg";
+    };
+  }
+  {
+    goPackagePath = "golang.org/x/sys";
+    fetch = {
+      type = "git";
+      url = "https://go.googlesource.com/sys";
+      rev = "86b910548bc16777f40503131aa424ae0a092199";
+      sha256 = "1z8l2wp27q0bd4nc46j31lc7cr6kiw52zi6ix3i121pd3rcyrw44";
     };
   }
   {
