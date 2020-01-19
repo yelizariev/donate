69726e4625d4c25788cabbbaa520835d039fbf20
Mikhail Klementev <blame@dumpstack.io>
2020-01-19 10:48:19 +0000

    Nix: fix runtime library dependency

--- default.nix
+++ default.nix
@@ -11,6 +11,18 @@ in with pkgs; buildGoPackage rec {
   src = ./.;
   goDeps = ./deps.nix;
 
+  # TODO
+  # The problem here is that one dependency relies on the native
+  # library so, build will be stopped because RPATH will contain
+  # /build/go/...
+  preFixup = let libPath = stdenv.lib.makeLibraryPath [ glibc ]; in ''
+    RUNTIME=/build/go/src/github.com/wasmerio/go-ext-wasm/wasmer/libwasmer_runtime_c_api.so
+    mkdir $bin/lib
+    cp $RUNTIME $bin/lib/
+    patchelf --set-rpath $bin/lib:${libPath} "$bin/bin/${pname}"
+    patchelf --set-rpath $bin/lib:${libPath} "$bin/bin/${pname}-ci"
+  '';
+
   postFixup = ''
     wrapProgram $bin/bin/${pname} \
       --prefix PATH : "${lib.makeBinPath [ which electrum ]}"
